edb7ca3acb8151bda48d1d7ceba6d4c6084f75f5
fogus <mefogus@gmail.com>
2023-10-06 12:59:08 -0400

    Added a simple project.clj file for use in packaging

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject com.github.clojure/data.alpha.replicant-client "master-SNAPSHOT"
+  :description "A Clojure library providing client-side implementations of Clojure datastructures served by replicant-server."
+  :dependencies [])
